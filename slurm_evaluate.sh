fb986a0b2526e942b545defc58be0dcc4c5d14f4
leyangwen <wenleyan@umich.edu>
2024-09-04 23:18:13 -0400

    New infer - not tilt corrected

--- slurm_evaluate.sh
+++ slurm_evaluate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash - l
+#!/bin/bash -l
 #SBATCH --job-name=MB_eval
 #SBATCH --output=output_slurm/eval_log.txt
 #SBATCH --error=output_slurm/eval_error.txt
@@ -33,46 +33,43 @@ echo "cpu-2, gpu-1, mem-20"
 #config_file="configs/pose3d/MB_ft_h36m.yaml"
 #config_file="configs/pose3d/MB_train_VEHSR3.yaml"
 #config_file="configs/pose3d/MB_ft_VEHSR3_6DPose.yaml"
-#config_file="configs/pose3d/RTMPose_exp/MB_ft_VEHS_config6.yaml"
+config_file="configs/pose3d/RTMPose_exp/MB_ft_VEHS_config6.yaml"
 #config_file="configs/pose3d/RTMPose_exp/MB_ft_inference.yaml"
 #config_file="configs/pose3d/RTMPose_exp/MB_ft_VEHS_tilt_correct.yaml"
-config_file="configs/pose3d/hand/MB_train_Rokoko.yaml"
+#config_file="configs/pose3d/hand/MB_train_Rokoko.yaml"
 
 # Checkpoint
 #checkpoint_bin="checkpoint/pose3d/MB_train_h36m/best_epoch.bin"
 #checkpoint_bin="checkpoint/pose3d/MB_ft_VEHSR3_6DPose/best_epoch.bin"
 #checkpoint_bin="checkpoint/pose3d/FT_MB_release_MB_ft_h36m/best_epoch.bin"
-#checkpoint_bin="checkpoint/pose3d/FT_RTM_VEHS_config6/best_epoch.bin"
+checkpoint_bin="checkpoint/pose3d/FT_RTM_VEHS_config6/best_epoch.bin"
 #checkpoint_bin="checkpoint/pose3d/FT_RTM_VEHS_config6_GT2d_true/best_epoch.bin"
 #checkpoint_bin="checkpoint/pose3d/FT_RTM_VEHS_tilt_corrected/best_epoch.bin"
-checkpoint_bin="checkpoint/pose3d/MB_train_Rokoko_hand_21/latest_epoch.bin"
+#checkpoint_bin="checkpoint/pose3d/MB_train_Rokoko_hand_21/latest_epoch.bin"
 
 
 echo "config_file: $config_file"
 echo "checkpoint_bin: $checkpoint_bin"
 
-# evaluate - train.py
+# evaluate - train.py [pose3d]
 python -u train.py \
 --config "$config_file" \
---wandb_project "MotionBert_eval_Hand" \
---wandb_name "Rokoko_1"  \
+--wandb_project "MotionBert_eval" \
+--wandb_name "Redo-RTM24-RTM_input_MB_ft_validate"  \
 --note "" \
---out_path "experiment/handPose/Rokoko_1" \
---test_set_keyword test \
+--out_path "experiment/RTM2D_ft/Redo-RTM24-RTM_input_MB_ft_validate" \
+--test_set_keyword validate \
 --evaluate "$checkpoint_bin" \
 #--save_trace \
 
-# inference only
-#python -u infer3d_train.py \
+## evaluate - train.py [hand]
+#python -u train.py \
 #--config "$config_file" \
-#--wandb_project "MotionBert_eval" \
-#--wandb_name "RTM_input_MB_ft_inference_industry_tilt_corrected" \
-#--note "save pose output" \
-#--out_path "experiment/RTM2D_ft/industry_RTM2d_tilt_corrected" \
-#--test_set_keyword validate \
-#--evaluate "$checkpoint_bin"
-
-#> "output_slurm/eval_${SLURM_JOB_ID}_output.out"
-
+#--wandb_project "MotionBert_eval_Hand" \
+#--wandb_name "Rokoko_1"  \
+#--note "" \
+#--out_path "experiment/handPose/Rokoko_1" \
+#--test_set_keyword test \
+#--evaluate "$checkpoint_bin" \
 
 # (config change 4 location)
