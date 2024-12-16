c162e239be8c604839f19754831f4606188c6383
leyangwen <wenleyan@umich.edu>
2024-12-16 14:17:42 -0500

    mesh 17 and 66

--- lib/data/dataset_mesh.py
+++ lib/data/dataset_mesh.py
@@ -30,7 +30,7 @@ class SMPLDataset(Dataset):
             if "VEHS" in args.dt_file_h36m:  # 17 joints but using VEHS7M data
                 datareader = DataReaderVEHSR3(n_frames=self.clip_len, sample_stride=args.sample_stride, data_stride_train=args.data_stride,
                                               data_stride_test=self.clip_len, dt_root=args.data_root,
-                                              dt_file=args.dt_file_VEHS7M, test_set_keyword=args.test_set_keyword, num_joints=args.num_joints)
+                                              dt_file=args.dt_file_h36m, test_set_keyword=args.test_set_keyword, num_joints=args.num_joints)
             else:
                 datareader = DataReaderH36M(n_frames=self.clip_len, sample_stride=args.sample_stride, data_stride_train=args.data_stride, data_stride_test=self.clip_len, dt_root=args.data_root, dt_file=args.dt_file_h36m)
         elif dataset=="coco":
